c32013a64886ae49350ca288754e7567a6761fe4
xiaojia <624990742@qq.com>
2025-07-26 14:19:34 +0800

    添加计算属性，和ref、reactive的用法。

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,58 @@
 <script setup>
-defineProps({
+import { reactive , ref , computed } from 'vue'
+
+// 响应式对象（适合对象、数组等）
+const counter = reactive({ count: 0 })
+
+// 响应式基本类型（适合字符串、数字、布尔值）
+const message = ref('Hello World!')
+
+console.log(message.value)
+message.value = '修改'
+
+
+// 定义 props
+const props = defineProps({
   msg: {
     type: String,
-    required: true,
-  },
+    required: true
+  }
 })
+
+
+// 定义两个响应式变量，分别代表姓和名
+const firstName = ref('Tom')
+const lastName = ref('Hanks')
+
+// 创建一个计算属性 fullName，动态拼接姓和名
+const fullName = computed(() => {
+  return `${firstName.value} ${lastName.value}`
+})
+
+
+
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+  <h1>{{ message }}</h1>
+  <p>当前计数是：{{ counter.count }}</p>
+  <br></br>
+  <p>当前用户的全名是：{{ fullName }}</p>
+
+
+
+  <!-- <h1>{{ message.split('').reverse().join('') }}</h1> -->
+
+
+
+  <!-- <div class="greetings">
+    <h1 class="green">{{ props.msg }}</h1>
     <h3>
       You’ve successfully created a project with
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
-  </div>
+  </div> -->
 </template>
 
 <style scoped>
