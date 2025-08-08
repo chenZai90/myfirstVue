6f371d34a665f8bac9746d6fb25e153152940dec
xiaojia <624990742@qq.com>
2025-08-08 19:11:31 +0800

    添加一些学习代码

--- src/vueDemo/classAndStyle.vue
+++ src/vueDemo/classAndStyle.vue
@@ -18,40 +18,20 @@ const activeClass = ref('active')
 const errorClass = ref('text-danger')
 
 
+const name = ref('Vue 用户')
 
+// 方法定义，event 是原生 DOM 事件
+function greet(event) {
+  alert(`你好，${name.value}！`)
+  alert(`你点击了标签：${event.target.tagName}`)
+}
 
 </script>
 
-<template >
-<div class="static" :class="{ active: isActive, 'text-danger': hasError}"></div>
+<template>
 
-<div :class="classTestObject"></div>
-
-<div :class="[activeClass,errorClass]"></div>
-
-<div :class="[isActive ? activeClass : '',errorClass]"></div>
-
-<div :class="[{[activeClass]: isActive}, errorClass]"></div>
-
-<!-- <p class="foo bar">Hi nihao </p>
-<myCompent class="baz boo"> -->
-
-    <p :class="$attrs.class">Hi!你好</p>
-    <span>This is a child componet</span>
-
-<div v-if="type === 'A'">
-  A
-</div>
-<div v-else-if="type === 'B'">
-  B
-</div>
-<div v-else-if="type === 'C'">
-  C
-</div>
-<div v-else>
-  Not A/B/C
-</div>
-
-
+  <template>
+  <button @click="greet">问候</button>
+</template>
 
 </template>
